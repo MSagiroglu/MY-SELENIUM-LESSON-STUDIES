d212d9c559f4a8dabbc89a0cd66f08551e4a16e4
MSagiroglu <mustafasagiroglu0645@gmail.com>
2023-05-27 23:29:15 +0300

    27.05.2023 new selenium lesson study 2

---
+++ src/test/java/techproed/day02_DriverMethods/C04_Homework1.java
@@ -0,0 +1,16 @@
+package techproed.day02_DriverMethods;
+
+public class C04_Homework1 {
+    /*
+    Yeni bir class olusturalim (Homework)
+ChromeDriver kullanarak, facebook sayfasina gidin ve sayfa basliginin (title) “facebook”
+oldugunu dogrulayin (verify), degilse dogru basligi yazdirin.
+Sayfa URL’inin “facebook” kelimesi icerdigini dogrulayin, icermiyorsa “actual” URL’i
+yazdirin.
+https://www.walmart.com/ sayfasina gidin.
+Sayfa basliginin “Walmart.com” icerdigini dogrulayin.
+Tekrar “facebook” sayfasina donun
+Sayfayi yenileyin
+Sayfayi tam sayfa (maximize) yapin  9.Browser’i kapatin
+     */
+}
