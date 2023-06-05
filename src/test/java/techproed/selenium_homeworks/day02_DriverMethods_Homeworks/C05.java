f54c435d2e7976c27d9c874461e2be8bc890caeb
Mustafa SAĞIROĞLU <mustafasagiroglu0645@gmail.com>
2023-06-05 10:17:07 +0300

    05/06/2023-3

--- src/test/java/techproed/selenium_homeworks/day02_DriverMethods_Homeworks/C05.java
+++ src/test/java/techproed/selenium_homeworks/day02_DriverMethods_Homeworks/C05.java
@@ -3,6 +3,7 @@ package techproed.selenium_homeworks.day02_DriverMethods_Homeworks;
 public class C05 {
     public static void main(String[] args) {
         System.out.printf("dsad");
+        System.out.println();
 
     }
 }
